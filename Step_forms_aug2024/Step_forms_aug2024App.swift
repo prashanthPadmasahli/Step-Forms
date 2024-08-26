ed326b23de5277df096d28c8d2309ab3643709ec
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-08-26 22:55:56 +0530

    Initial Commit

---
+++ Step_forms_aug2024/Step_forms_aug2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Step_forms_aug2024App.swift
+//  Step_forms_aug2024
+//
+//  Created by mac on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_forms_aug2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
